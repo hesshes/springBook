d2fdd918bb5e03393bda1d57e815e84386d974ce
tae-woo Kim <hesshes@naver.com>
2023-01-15 18:05:46 +0900

    dev2 Single Tone Obj pages

--- src/com/springbook/dao/DaoFactory.java
+++ src/com/springbook/dao/DaoFactory.java
@@ -1,9 +1,42 @@
 package com.springbook.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/*
+ * step7: context 적용 전
 public class DaoFactory {
+	 step6 : 오브젝트 팩토리 활용전 코드
+	 * public UserDao userDao() { ConnectionMaker connectionMaker = new
+	 * DConnectionMaker(); UserDao userDao = new UserDao(connectionMaker); return
+	 * userDao; }
+	 
+	
+	public UserDao userDao(ConnectionMaker connectionMaker) {
+		return new UserDao(connectionMaker());
+	}
+	
+	// 중복 제거를 위한 코드
+	public ConnectionMaker connectionMaker() {
+		return new DConnectionMaker();
+	}
+}
+*/
+
+@Configuration
+public class DaoFactory {
+
 	public UserDao userDao() {
-		ConnectionMaker connectionMaker = new DConnectionMaker();
-		UserDao userDao = new UserDao(connectionMaker);
-		return userDao;
+		return new UserDao();
+	}
+
+	@Bean
+	public UserDao userDao(ConnectionMaker connectionMaker) {
+		return new UserDao(connectionMaker());
+	}
+
+	@Bean
+	public ConnectionMaker connectionMaker() {
+		return new DConnectionMaker();
 	}
 }
--- src/com/springbook/main/SpringBook.java
+++ src/com/springbook/main/SpringBook.java
@@ -2,6 +2,9 @@ package com.springbook.main;
 
 import java.sql.SQLException;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import com.springbook.dao.ConnectionMaker;
 import com.springbook.dao.DConnectionMaker;
 import com.springbook.dao.DaoFactory;
@@ -20,16 +23,42 @@ public class SpringBook {
 		 * DConnectionMaker(); //UserDao dao = new UserDao(connectionMaker);
 		 */		
 		
-		UserDao dao = new DaoFactory().userDao();
+		/* 
+		 * //step6 : 관계설정 분리 적용한 코드
+		 * UserDao dao = new DaoFactory().userDao();
+		 */	
 		
-		User user = new User();
-		user.setId("bookspring");
-		user.setName("스프링2");
-		user.setPassword("spring");
-
-		dao.add(user);
-
-		System.out.println(user.getId() + " 등록 성공 ");
+		ApplicationContext ctx = new AnnotationConfigApplicationContext(DaoFactory.class);
+		UserDao dao = ctx.getBean("userDao", UserDao.class);
+		
+		/*
+		 * User user = new User(); user.setId("bookspring"); user.setName("스프링2");
+		 * user.setPassword("spring");
+		 * 
+		 * dao.add(user);
+		 * 
+		 * System.out.println(user.getId() + " 등록 성공 ");
+		 */
+		
+		DaoFactory df = new DaoFactory();
+		UserDao dao2 = df.userDao();
+		UserDao dao3 = df.userDao();
+		
+		System.out.println("직접 객체 생성 한 dao 객체");
+		System.out.println(dao2 + " dao2");
+		System.out.println(dao3 + " dao3");
+		System.out.println(dao2==dao3);
+		
+		UserDao dao4 = ctx.getBean("userDao", UserDao.class);
+		UserDao dao5 = ctx.getBean("userDao", UserDao.class);
+		
+		// 스프링의 싱글톤 패턴은 디자인 패턴에서 나오는 싱글톤 패턴과 비슷한 개념이지만, 그 구현 방법이 확연히 다름
+		System.out.println("스프링 컨텍스트를 사용해서 만든 dao 객체");
+		System.out.println(dao + " dao");
+		System.out.println(dao4 + " dao4");
+		System.out.println(dao5 + " dao5");		
+		System.out.println((dao == dao4));
+		
 
 	}
 }
