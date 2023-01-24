e7bba1b5b96683984b66b22bfc87fb9725d1378e
tae-woo Kim <hesshes@naver.com>
2023-01-24 11:31:00 +0900

    20230124 re

--- src/com/springbook/dao/UserDao.java
+++ src/com/springbook/dao/UserDao.java
@@ -962,18 +962,24 @@ public class UserDao {
 
 public class UserDao {
 
-	private JdbcContext jdbcContext;
 	private DataSource dataSource;
+	
+	private JdbcContext jdbcContext;
 
 	public void setDataSource(DataSource dataSource) {
+		
+		this.jdbcContext = new JdbcContext();
+		
+		this.jdbcContext.setDataSource(dataSource);
+		
 		this.dataSource = dataSource;
+		
 	}
 
-	/*
-	 * public void setJdbcContext(JdbcContext jdbcContext) { this.jdbcContext =
-	 * jdbcContext; }
-	 */
-	
+	public void setJdbcContext(JdbcContext jdbcContext) {
+		this.jdbcContext = jdbcContext;
+	}
+
 	public void add(final User user) throws SQLException {
 
 		this.jdbcContext.workWithStatementStrategy(new StatementStrategy() {
