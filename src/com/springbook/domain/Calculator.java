fcecfd8b24ec36e1f5b6e7d4a8922ad2385a3eea
tae-woo Kim <hesshes@naver.com>
2023-01-24 19:36:36 +0900

    20230124 dev2  // temple , callback  study update

--- src/com/springbook/domain/Calculator.java
+++ src/com/springbook/domain/Calculator.java
@@ -4,25 +4,97 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+import com.springbook.template.BufferedReaderCallBack;
+import com.springbook.template.LineCallBack;
+
 public class Calculator {
+
+	/*
+	 * 템플릿 미 적용 코드 public Integer calcSum(String filePath) throws IOException {
+	 * 
+	 * BufferedReader br = null;
+	 * 
+	 * try { br = new BufferedReader(new FileReader(filePath)); Integer sum = 0;
+	 * String line = null; while ((line = br.readLine()) != null) { sum +=
+	 * Integer.valueOf(line); } return sum;
+	 * 
+	 * } catch (IOException e) {
+	 * 
+	 * System.out.println(e.getMessage()); throw e;
+	 * 
+	 * } finally { if (br != null) { try { br.close(); } catch (IOException e) {
+	 * System.out.println(e.getMessage()); } } } }
+	 */
+
+	/*
+	 * 템플릿 코드 적용 1차 public Integer calcSum(String filePath) throws IOException {
+	 * 
+	 * BufferedReaderCallBack sumCallBack = new BufferedReaderCallBack() {
+	 * 
+	 * public Integer doSomethingWithReader(BufferedReader br) throws IOException {
+	 * Integer sum = 0; String line = null; while ((line = br.readLine()) != null) {
+	 * sum += Integer.valueOf(line); } return sum; } };
+	 * 
+	 * return fileReadTemplate(filePath, sumCallBack); }
+	 * 
+	 * public Integer calcMultiply(String filePath) throws IOException {
+	 * 
+	 * BufferedReaderCallBack multiplyCallBack = new BufferedReaderCallBack() {
+	 * 
+	 * public Integer doSomethingWithReader(BufferedReader br) throws IOException {
+	 * Integer multiply = 1; String line = null; while ((line = br.readLine()) !=
+	 * null) { multiply += Integer.valueOf(line); } return multiply; } };
+	 * 
+	 * return fileReadTemplate(filePath, multiplyCallBack); }
+	 * 
+	 * public Integer fileReadTemplate(String filePath, BufferedReaderCallBack
+	 * callBack) throws IOException {
+	 * 
+	 * BufferedReader br = null; try { br = new BufferedReader(new
+	 * FileReader(filePath)); int ret = callBack.doSomethingWithReader(br); return
+	 * ret;
+	 * 
+	 * } catch (IOException e) { System.out.println(e.getMessage()); throw e; }
+	 * finally { if (br != null) { try { br.close(); } catch (IOException e) {
+	 * System.out.println(e.getMessage()); } } } }
+	 */
+
 	public Integer calcSum(String filePath) throws IOException {
 
-		BufferedReader br = null;
+		LineCallBack sumCallBack = new LineCallBack() {
+			public Integer doSomethingWithLine(String line, Integer value) {
+				return value + Integer.valueOf(line);
+			}
+		};
 
+		return lineReadTemplate(filePath, sumCallBack, 0);
+	}
+
+	public Integer calcMultiply(String filePath) throws IOException {
+
+		LineCallBack multiplyCallBack = new LineCallBack() {
+
+			public Integer doSomethingWithLine(String line, Integer value) {
+				return value * Integer.valueOf(line);
+			}
+		};
+		return lineReadTemplate(filePath, multiplyCallBack, 1);
+	}
+
+	public Integer lineReadTemplate(String filePath, LineCallBack callBack, int initVal) throws IOException {
+
+		BufferedReader br = null;
 		try {
 			br = new BufferedReader(new FileReader(filePath));
-			Integer sum = 0;
+			Integer res = initVal;
 			String line = null;
 			while ((line = br.readLine()) != null) {
-				sum += Integer.valueOf(line);
+				res = callBack.doSomethingWithLine(line, res);
 			}
-			return sum;
-
+			return res;
 		} catch (IOException e) {
-
 			System.out.println(e.getMessage());
 			throw e;
-
 		} finally {
 			if (br != null) {
 				try {
---
+++ src/com/springbook/template/CalcSumTest.java
@@ -0,0 +1,43 @@
+package com.springbook.template;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.springbook.domain.Calculator;
+
+public class CalcSumTest {
+	
+	/* 템플릿 코드 적용 전
+	 * @Test public void sumOfNumbers() throws IOException {
+	 * 
+	 * Calculator calculator = new Calculator(); int sum =
+	 * calculator.calcSum(getClass().getResource("/numbers.txt").getPath());
+	 * assertThat(sum, is(10)); }
+	 */
+	
+	Calculator calculator;
+	String numFilePath;
+	
+	@Before
+	public void setUp() {
+		this.calculator = new Calculator();
+		this.numFilePath = getClass().getResource("/numbers.txt").getPath();
+	}
+
+	@Test
+	public void sumOfNumbers() throws IOException {
+		assertThat(calculator.calcSum(this.numFilePath), is(10));
+	}
+	
+	@Test
+	public void multiplyOfNumbers() throws IOException {
+		assertThat(calculator.calcMultiply(this.numFilePath), is(10));
+	}
+}
+
+	
